c2464a553d3c4f100a6957dc21ceaede54a3cb68
Qrisssss <105015417+Qrisssss@users.noreply.github.com>
2024-06-30 11:42:34 +0300

    minor to Update test.js

--- test.js
+++ test.js
@@ -11,30 +11,6 @@ function calculateInterest() {
                             document.getElementById('result').innerText = "Please enter valid numbers.";
                                     return;
                                         }
-
-                let interest = principal * (rate / 100) * time;
-                 let totalAmount = principal + interest;
-
-                            document.getElementById('result').innerText = 
-                                   `Interest: KSH${interest.toFixed(2)}\nTotal Amount: KSH${totalAmount.toFixed(2)}\nhello brp`;
-                                        } let submit = document.getElementById('submit');
-                                                            {
-                                   
-                                                            };
-function dark(){
-    document.documentElement.classList.toggle('darklight');
-}
-
-function calculateInterest() {
-        let principal = parseFloat(document.getElementById('principal').value);
-            let rate = parseFloat(document.getElementById('rate').value);
-                let time = parseFloat(document.getElementById('time').value);
-
-                    if (isNaN(principal) || isNaN(rate) || isNaN(time)) {
-                            document.getElementById('result').innerText = "Please enter valid numbers.";
-                                    return;
-                                        }
-
                 let interest = principal * (rate / 100) * time;
                 let totalAmount = principal + interest;
                 let daily = interest / 365;
