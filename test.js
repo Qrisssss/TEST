aa3a88aa4fa73da8b69b59a8fd9f2f8a5b7462d0
Qrisssss <105015417+Qrisssss@users.noreply.github.com>
2024-06-30 11:39:13 +0300

    Update to daily interest calc

--- test.js
+++ test.js
@@ -21,6 +21,31 @@ function calculateInterest() {
                                                             {
                                    
                                                             };
+function dark(){
+    document.documentElement.classList.toggle('darklight');
+}
+
+function calculateInterest() {
+        let principal = parseFloat(document.getElementById('principal').value);
+            let rate = parseFloat(document.getElementById('rate').value);
+                let time = parseFloat(document.getElementById('time').value);
+
+                    if (isNaN(principal) || isNaN(rate) || isNaN(time)) {
+                            document.getElementById('result').innerText = "Please enter valid numbers.";
+                                    return;
+                                        }
+
+                let interest = principal * (rate / 100) * time;
+                let totalAmount = principal + interest;
+                let daily = interest / 365;
+                let monthly = interest / 12;
+                let weekly = daily * 7;
+
+        document.getElementById('result').innerText =
+        `Interest: KSH${interest.toFixed(2)}\n Daily Interest:KSH${daily.toFixed(2)} \n weekly Interest:KSH${weekly.toFixed(2)}\n Monthly Interest:KSH${monthly.toFixed(2)}\n TotalAmount: KSH${totalAmount.toFixed(2)}\n`;} let submit = document.getElementById('submit');
+                                              {
+                                   
+                                                            };
 
 
 
